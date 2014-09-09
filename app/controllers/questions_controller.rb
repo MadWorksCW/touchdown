adcdce09a419c2f3f578db17dfd5765002842a95
Brian Samson <brian@briansamson.com>
2014-09-09 15:09:08 -0500

    easier question ordering

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -15,6 +15,7 @@ class QuestionsController < ApplicationController
   # GET /questions/new
   def new
     @question = Question.new
+    @question.order = Question.count + 1
   end
 
   # GET /questions/1/edit
