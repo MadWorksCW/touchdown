79665c1f0776727fc607e31108e5d1e4ff749e3a
Brian Samson <brian@briansamson.com>
2015-01-13 15:45:22 -0600

    redirect scorers to the app index page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,8 @@ class HomeController < ApplicationController
   def index
     if current_user.nil?
       @applications = Application.open
-    elsif current_user.admin?
-      redirect_to applications_path
     else
-      redirect_to application_responses_path(@application)
+      redirect_to applications_path
     end
   end
 end
