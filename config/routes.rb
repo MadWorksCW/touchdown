6c5b55885d66e299d2e2aec78ffce73f0d5ee2cd
Brian Samson <brian@briansamson.com>
2015-01-21 16:07:56 -0600

    fix response updating

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
   get 'dashboard', to: 'applications#index', as: 'dashboard'
-  resources :responses, only: [:show, :edit] do
+  resources :responses, only: [:show, :edit, :update] do
     get 'score', on: :member
     get 'results', on: :member
     get 'new', on: :collection, to: redirect('/')
